9287d5a908008df3939fcc9662f945f7489bad34
nekogato <05kcwong@gmail.com>
2019-03-30 15:39:55 +0800

    Updates

---
+++ precache-manifest.a3464226f990d3ce3de63565402ebf2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "b187eefcaace1c6db620",
+    "url": "/myapp/static/js/main.8a4719ae.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/myapp/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b187eefcaace1c6db620",
+    "url": "/myapp/static/css/main.d98bed6c.chunk.css"
+  },
+  {
+    "revision": "a1de60e19feb5eab09b07ccc63bd7881",
+    "url": "/myapp/index.html"
+  }
+];
